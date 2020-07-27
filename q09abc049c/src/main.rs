e8860b2c65ec79ddb973cf66a17d24f7dcfec66c
lambdalisue <lambdalisue@hashnote.net>
2020-07-28 03:50:30 +0900

    Add 9

---
+++ q09abc049c/src/main.rs
@@ -0,0 +1,79 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        #[allow(unused_mut)]
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        value: String,
+    };
+    let mut value = value;
+    loop {
+        let previous = value.clone();
+        value = remove_suffix(&value, "dreamer").to_string();
+        value = remove_suffix(&value, "dream").to_string();
+        value = remove_suffix(&value, "eraser").to_string();
+        value = remove_suffix(&value, "erase").to_string();
+        if value == "" {
+            println!("YES");
+            return;
+        }
+        if value == previous {
+            println!("NO");
+            return;
+        }
+    }
+}
+
+fn remove_suffix<'a>(s: &'a str, p: &str) -> &'a str {
+    if s.ends_with(p) {
+        &s[..s.len() - p.len()]
+    } else {
+        s
+    }
+}
