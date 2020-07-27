c4c9c485218e48f2e511b06f77c869232f32eae0
lambdalisue <lambdalisue@hashnote.net>
2020-07-28 03:05:59 +0900

    Add 8

---
+++ q08abc085c/src/main.rs
@@ -0,0 +1,70 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        #[allow(unused_mut)]
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn find(n: i32, v: i32) -> Option<(i32, i32, i32)> {
+    for a in 0..=n {
+        for b in 0..=(n - a) {
+            let c = n - a - b;
+            if a * 10000 + b * 5000 + c * 1000 == v {
+                return Some((a, b, c));
+            }
+        }
+    }
+    None
+}
+
+fn main() {
+    input! {
+        n: i32,
+        v: i32,
+    }
+    let (x, y, z) = find(n, v).unwrap_or((-1, -1, -1));
+    println!("{} {} {}", x, y, z)
+}
